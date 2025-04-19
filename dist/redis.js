e1dd80de4218a79501e60895770c4fefff1c5f4d
Himanshu Gupta <himanshugupta@Himanshus-MacBook-Air.local>
2025-04-19 13:01:52 +0530

    redis cloud provider added

--- dist/redis.js
+++ dist/redis.js
@@ -2,5 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.redis = void 0;
 const redis_1 = require("redis");
-exports.redis = (0, redis_1.createClient)();
+exports.redis = (0, redis_1.createClient)({
+    url: process.env.REDIS_URL,
+    socket: {
+        tls: true,
+    },
+});
 exports.redis.on("error", (err) => console.error("Redis Client Error", err));
